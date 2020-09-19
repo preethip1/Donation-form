e291c4a4dc746e05120b743b402335ce899251a2
preethip1 <preethiknot1998@gmail.com>
2020-09-19 22:22:59 +0530

    initial  commit

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+import Form from "./Form.svelte";
+</script>
+
+<main>
+<h1> DONATION FORM </h1>
+<Form/>
+<h3> Thank You! Your Support is greatly appreciated!</h3>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
---
+++ src/Form.svelte
@@ -0,0 +1,28 @@
+<script>
+  function submit() {
+    alert("Thanks!");
+  }
+</script>
+
+<div class="name">
+  Name <input type="text" placeholder="Enter your full name" />
+</div>
+<div class="phone-number">Phone Number <input type="number" /></div>
+<div class="email">
+  Email <input type="email" placeholder="Enter your Email ID" />
+</div>
+<div class="address">Address <input type="text" /></div>
+<div class="pincode">Pincode <input type="number" placeholder="Pincode" /></div>
+<div class="payment">
+  Payment: <input type="radio" name="payment" value="Cash" /> Cash <input
+    type="radio"
+    name="payment"
+    value="Cheque" /> Cheque <input
+    type="radio"
+    name="payment"
+    value="Online payment" /> Online Payment
+</div>
+<div class="amount">Amount <input type="number" placeholder="Rs." /></div>
+<div class="submit">
+  <button on:click={submit} placeholder="submit">Submit </button>
+</div>
